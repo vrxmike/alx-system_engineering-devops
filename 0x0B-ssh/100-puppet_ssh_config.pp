58acaf9ed18a615cc7a5558a5040e08286913f1e
vrxmike <alvinvrxmike@icloud.com>
2023-01-24 10:07:38 -0800

    updated-changes

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,26 @@
-#!/usr/bin/env bash
-# connect to a server Execute a command
+#!/usr/bin/env/bash
+# using puppet to make changes to the default ssh config file
+# so that one can conect to a server without typing a password
+#
+# Regex match explanation
+#
+# ^       beginning of the line
+# [#]*  atleast one hash character
+# [\s]*  zero or more white space characters
+# (?i)IdentityFile case insensitive "IdentityFile"
+# [\s]+ at least one whitespace character
+# ~/.ssh/id_rsa The ssh private key file path we want to replace
+# $      end of the line
 
 exec { 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config':
-	path	=> '/bin/'
+        path    => '/bin/'
 }
+# Regex match explanation
+#
+# ^       beginning of the line
+# [#]*  atleast one hash character
+# [\s]*  zero or mmore white space characters
+# (?i)PasswordAuthentication case insensititve "PasswordAuthentication"
+# [\s]+ at least one whitespace character
+# (yes|no) with the value "yes" or the value "no"
+# $      end of the line
