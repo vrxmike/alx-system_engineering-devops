104d273a73625667e502c2fe90e5f79b535614e4
vrxmike <alvinvrxmike@icloud.com>
2023-02-08 14:06:02 -0800

    0-Task-Fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,30 +1,23 @@
 #!/usr/bin/python3
-'''A script that gathers employee name completed
+"""A script that gathers employee name completed
 tasks and total number of tasks from an API
-'''
-
-import re
+"""
+import json
 import requests
 import sys
 
-REST_API = 'https://jsonplaceholder.typicode.com'
 
-if __name__ = '__main__':
-    if len(sys.argv) > 1:
-        if re.fullmatch(r'\d+', sys.argv[1]):
-            id = int(sys.argv[1])
-            emp_req = requests.get('{}/todos'.format(REST_API, id)).json()
-            task_req = requests.get('{}/todos'.format(REST_API)).json()
-            emp_name = emp_req.get('name')
-            tasks = list(filter(lambda x: x.get('userId') == id, task_req))
-            completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
-            print(
-                'Employee {} is done with tasks({}/{}):'.format(
-                    emp_name,
-                    len(completed_tasks),
-                    len(tasks)
-                )
-            )
-            if len(completed_tasks) > 0:
-                for task in completed_tasks:
-                    print('\t {}'.format(task.get('title')))
+if __name__ == '__main__':
+    r = requests.get("https://jsonplaceholder.typicode.com/users/" +
+                     sys.argv[1])
+    dict = json.loads(r.text)
+    name = dict.get('name')
+    resp = requests.get("https://jsonplaceholder.typicode.com/todos/" +
+                        "?userId=" + sys.argv[1])
+    todos = json.loads(resp.text)
+    tasks = len(todos)
+    completed = [task for task in todos if task.get('completed')]
+    done = len(completed)
+    print("Employee {} is done with tasks({}/{})".format(name, done, tasks))
+    for task in completed:
+        print("\t", task.get('title'))
