4e5c4f66a66ed450363f8534e61898a0830188e7
vrxmike <alvinvrxmike@icloud.com>
2023-02-08 16:13:35 -0800

    2-Task-upd

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -4,20 +4,16 @@ import json
 import requests
 import sys
 
-
 if __name__ == "__main__":
-    response = requests.get("https://jsonplaceholder.typicode.com/users/" +
-                            sys.argv[1])
-    dicti = json.loads(response.text)
-    username = dicti.get('username')
-    response = requests.get("https://jsonplaceholder.typicode.com/todos/" +
-                            "?userId=" + sys.argv[1])
-    todos = json.loads(response.text)
-    tasks = [task for task in todos]
-    ans = {sys.argv[1]: []}
-    for task in tasks:
-        task_dict = {"task": task.get('title'),
-                     'completed': task.get('completed'), 'username': username}
-        ans.get(sys.argv[1].append(task_dict)
-    with open(sys.argv[1] + ".json", "w") as f:
-        json.dump(ans, f)
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": username
+            } for t in todos]}, jsonfile)
