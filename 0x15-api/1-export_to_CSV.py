c2edf5ad867c1ba4603c216614c6fea93a620389
vrxmike <alvinvrxmike@icloud.com>
2023-02-10 10:58:16 -0800

    1-Task

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,7 +8,7 @@ import sys
 if __name__ == "__main__":
     user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json
+    user = requests.get(url + "users/{}".format(user_id)).json()
     username = user.get("username")
     todos = requests.get(url + "todos", params={"userid": user_id}).json()
 
